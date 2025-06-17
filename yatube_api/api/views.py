8b01f1030f62a5080c0b73bb6bceccf124f1ff9b
Ольга Пырьянова <pyryanova@mail.ru>
2025-06-17 09:31:40 +0300

    «2.0»

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import (
     IsAuthenticatedOrReadOnly,
 )
 
-from posts.models import Follow, Group, Post
+from posts.models import Group, Post
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
