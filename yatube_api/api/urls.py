39975968f65e86ba0122622e49e06625704d8b8a
Ольга Пырьянова <pyryanova@mail.ru>
2025-06-17 10:18:41 +0300

    «2.0»

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,21 +2,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
-from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from rest_framework.routers import DefaultRouter
 
 from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
-
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'groups', GroupViewSet, basename='group')
 router.register(r'follow', FollowViewSet, basename='follow')
-posts_router = NestedDefaultRouter(router, r'posts', lookup='post')
-posts_router.register(r'comments', CommentViewSet, basename='post-comments')
+
+# хотела сделать с NestedDefaultRouter, но у Практикума падают тесты, 
+# а было красиво..
+
+# from rest_framework_nested.routers import NestedDefaultRouter
+# posts_router = NestedDefaultRouter(router, r'posts', lookup='post')
+# posts_router.register(r'comments', CommentViewSet, basename='post-comments')
+
+router.register(
+    r'posts/(?P<post_pk>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include(posts_router.urls)),
+    # path('v1/', include(posts_router.urls)),  # отключено
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
